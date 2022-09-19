d512c0c722b0a2acc142fc6e82621ab98a15b6bd
Tim <myieye@gmail.com>
2022-09-19 14:57:29 +0200

    Temp

--- playwright.config.ts
+++ playwright.config.ts
@@ -28,7 +28,7 @@ const config: PlaywrightTestConfig = {
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : 1,
+  //workers: 8,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   outputDir: 'test-results', // referenced in pull-request.yml
   reporter: process.env.CI ? [['github'], ['list']] : [['html', {outputFolder: 'test-results/_html-report'}]],
