a9f50a522ef48a0c34ac596c30242793e37e92e6
Tim Haasdyk <tim_haasdyk@sil.org>
2022-12-06 14:16:05 +0100

    Always save test-results This way we can compare failed and successful runs and also require results to be found, so that we notice if the upload in GHA breaks.

--- test/e2e/playwright.config.ts
+++ test/e2e/playwright.config.ts
@@ -50,8 +50,8 @@ const config: PlaywrightTestConfig = {
     // baseURL: 'http://localhost:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'retain-on-failure',
-    screenshot: 'only-on-failure',
+    trace: 'on',
+    screenshot: 'on',
   },
 
   /* Configure projects for major browsers */
